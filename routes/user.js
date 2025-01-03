62fa6962033e60e365ccd89d90c3bb0e025463ee
NikosKourkoulos1 <kourkoulosnikolaos@gmail.com>
2025-01-03 10:48:46 +0200

    updated package.json

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,16 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+// GET Method - Fetch all users
+router.get('/users', async (req, res) => {
+    try {
+      const users = await User.find(); 
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json({ message: 'Server error', error: err.message });
+    }
+  });
+
 // Get User Details
 router.get('/:id', async (req, res) => {
     try {
